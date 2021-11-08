7e16558e82ad3a4d20b16829899f5fa06944124d
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-11-08 22:04:49 +0500

    tmp

--- tests/test_squash.py
+++ tests/test_squash.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from test_command_base import TestCommand
 from commands.squash import SquashCommand
+from commands.add import AddCommand
+from commands.commit import CommitCommand
 
 
 class TestSquash(TestCommand):
@@ -14,7 +16,19 @@ class TestSquash(TestCommand):
             self.repository, "commit_hash")
 
     # def test_squash(self):
-    #     #todo
+    #     Path('file').touch()
+    #     AddCommand().execute('file')
+    #     CommitCommand().execute('initial')
+    #     initial_commit = self.repository.current_commit
+    #     with open(Path('file'), 'w') as f:
+    #         f.write('text')
+    #     AddCommand().execute('file')
+    #     CommitCommand().execute('second')
+    #     SquashCommand().execute(1)
+    #     with open(Path('file')) as f:
+    #         assert 'text' == f.readline()
+    #     assert initial_commit == self.repository.current_commit
+    #     Path('file').unlink()
 
     # def test_cannot_squash_if_wrong_number_of_params(self):
 
