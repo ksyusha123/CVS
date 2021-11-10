31d20d79af75b2736a7712fa6a2333fe62d217fe
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-11-10 17:51:51 +0500

    test compressor

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -6,17 +6,34 @@ from compressor import Compressor
 
 class TestCompressor(unittest.TestCase):
 
-    def test_decompress_small_file(self):
-        file = Path('file')
-        with open(file, 'w') as f:
-            f.write('small text')
-        compressed_content = Compressor().compress_file(file)
-        compressed_file = Path('compressed')
-        with open(compressed_file, 'wb') as compressed:
-            compressed.write(compressed_content)
-        decompressed_content = Compressor().decompress_file(compressed_file)
-        assert 'small text' == decompressed_content.decode()
-        file.unlink()
-        compressed_file.unlink()
+    @classmethod
+    def setUpClass(cls):
+        cls.file = Path('file')
+        cls.compressed_file = Path('compressed')
 
-    # def test_decompress_big_file(self):
+    @classmethod
+    def tearDownClass(cls):
+        cls.file.unlink()
+        cls.compressed_file.unlink()
+
+    def test_decompress_small_file(self):
+        with open(self.file, 'w') as f:
+            f.write('small text')
+        compressed_content = Compressor().compress_file(self.file)
+        with open(self.compressed_file, 'wb') as compressed:
+            compressed.write(compressed_content)
+        decompressed_content = Compressor().decompress_file(
+            self.compressed_file)
+        assert 'small text' == decompressed_content.decode()
+
+    def test_decompress_big_file(self):
+        with open(self.file, 'w') as f:
+            for _ in range(1000):
+                f.write("abababababab")
+        compressed_content = Compressor().compress_file(self.file)
+        with open(self.compressed_file, 'wb') as compressed:
+            compressed.write(compressed_content)
+        decompressed_content = Compressor().decompress_file(
+            self.compressed_file)
+        with open(self.file, 'rb') as f:
+            assert f.read() == decompressed_content
